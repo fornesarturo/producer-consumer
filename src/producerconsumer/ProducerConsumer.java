1b8a52eda4aad920a665c43a0f3f497792f6f205
Miguel Montoya <miguel.mzbi@gmail.com>
2018-04-05 16:57:40 -0500

    GUI Prototype

--- src/producerconsumer/ProducerConsumer.java
+++ src/producerconsumer/ProducerConsumer.java
@@ -2,8 +2,29 @@
 package producerconsumer;
 
 public class ProducerConsumer {
+    
 
+    @SuppressWarnings("empty-statement")
     public static void main(String[] args) {
+        
+        GUI gui = new GUI();
+        gui.setVisible(true);
+        javax.swing.JButton startButton = gui.getStartButton();
+        
+        startButton.addActionListener((java.awt.event.ActionEvent evt) -> {
+            startProgram(gui);
+        });
+        
+        
+    }
+    
+    public static void startProgram(GUI gui) {
+        int minRange = gui.getMinRange();
+        int maxRange = gui.getMaxRange();
+        int consumerQty = gui.getConsumerQty();
+        int producerQty = gui.getProducerQty();
+        long consumerMillis = gui.getConsumerMillis();
+        long producerMillis = gui.getProducerMillis();
         
         Buffer buffer = new Buffer();
         
